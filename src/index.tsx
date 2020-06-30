08be328f0c6555ac6953f42c7ec0d377a1197bc1
Løvgren, Dag Erik <dagerikhl@gmail.com>
2020-06-30 21:18:28 +0200

    Use experimental React to enable concurrent mode + Update React types

--- src/index.tsx
+++ src/index.tsx
@@ -9,13 +9,14 @@ import * as serviceWorker from "./serviceWorker";
 import "boxicons/css/boxicons.min.css";
 import "./index.css";
 
-ReactDOM.render(
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.unstable_createRoot(root).render(
   <React.StrictMode>
     <Providers>
       <App />
     </Providers>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
