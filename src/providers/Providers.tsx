3fc59c139a966ef3e3505f6e1aa8703886d5b8fc
Løvgren, Dag Erik <dagerikhl@gmail.com>
2020-07-02 16:24:22 +0200

    Make Providers generate provider tree from array of providers

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -3,8 +3,13 @@ import React from "react";
 import { FirebaseProvider } from "./FirebaseProvider";
 import { PersonalizationProvider } from "./PersonalizationProvider";
 
-export const Providers: React.FC = ({ children }) => (
-  <PersonalizationProvider>
-    <FirebaseProvider>{children}</FirebaseProvider>
-  </PersonalizationProvider>
-);
+const providers = [PersonalizationProvider, FirebaseProvider].reverse();
+
+export const Providers: React.FC = ({ children }) => {
+  let providerTree: React.ReactNode = children;
+  for (const provider of providers) {
+    providerTree = React.createElement(provider, null, providerTree);
+  }
+
+  return <>{providerTree}</>;
+};
